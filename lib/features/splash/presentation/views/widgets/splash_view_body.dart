de9e39fcf1cbc380cd84ff5b4a8559163f744742
m9stafa05 <m9stafa05@gmail.com>
2025-07-06 05:47:37 +0300

    Build Splash Screen by add the logo and small description

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,24 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SvgPicture.asset(AssetsData.logo, width: 200),
+        Center(
+          child: Text(
+            'Read Free Books',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ],
+    );
   }
 }
