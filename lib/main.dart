2396450d35aff2a947bb78a3bad5af91cf8c1c1c
m9stafa05 <m9stafa05@gmail.com>
2025-07-06 05:19:50 +0300

    Change the them to the dark & Change the default background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constans.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -7,8 +8,6 @@ Api =>
       https://www.postman.com/restless-space-875499/workspace/students/collection/14058212-d3c30656-4efa-4166-8405-86655985d94d?action=share&creator=14058212
 Design =>
       https://drive.google.com/file/d/1Y-noGizbczmGe2qEiZ4z9PDyIOoy1csS/view?usp=sharing
-
-      presentation
  */
 void main() {
   runApp(const BooklyApp());
@@ -19,6 +18,12 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(home: SplashView());
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
   }
 }
