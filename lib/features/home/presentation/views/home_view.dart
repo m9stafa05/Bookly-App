6d74d5ea261010c5f23d8fdf0496c83b1d61d613
m9stafa05 <m9stafa05@gmail.com>
2025-07-07 03:55:31 +0300

    Add Home ViewBody

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,9 +1,10 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/howe_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Column());
+    return const Scaffold(body: HoweViewBody());
   }
 }
