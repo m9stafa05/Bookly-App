811cad8715ba0abb42558cb8770291fec722d10d
m9stafa05 <m9stafa05@gmail.com>
2025-07-07 03:24:10 +0300

    correct he import files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/constans.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
