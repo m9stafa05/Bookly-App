f766a13aaabfff0b8fa76c8947039f365c65b980
m9stafa05 <m9stafa05@gmail.com>
2025-07-07 03:56:06 +0300

    Crate Home view body

---
+++ lib/features/home/presentation/views/widgets/howe_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HoweViewBody extends StatelessWidget {
+  const HoweViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(children: [CustomAppBar()]);
+  }
+}
+
