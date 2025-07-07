ec457af3cd9c8103dd2081067dd3c3dbd12ac038
m9stafa05 <m9stafa05@gmail.com>
2025-07-07 05:05:24 +0300

    Refactor & Add title

--- lib/features/home/presentation/views/widgets/howe_view_body.dart
+++ lib/features/home/presentation/views/widgets/howe_view_body.dart
@@ -1,14 +1,25 @@
+import 'package:bookly_app/core/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_bokks_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HoweViewBody extends StatelessWidget {
   const HoweViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomAppBar(), FeaturedBooksListView()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          Gap(50),
+          Text('Best Seller', style: Styles.titleMedium),
+        ],
+      ),
     );
   }
 }
