ebc8ee998b0f64de9834884cbd35bf9aaaecf8e5
m9stafa05 <m9stafa05@gmail.com>
2025-07-08 22:16:38 +0300

    Add list view & use custom list view

--- lib/features/home/presentation/views/widgets/howe_view_body.dart
+++ lib/features/home/presentation/views/widgets/howe_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -11,25 +11,36 @@ class HoweViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CustomAppBar(),
-          const FeaturedBooksListView(),
-          const Gap(50),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18.copyWith(
-              fontFamily: kMontserrat,
-              fontWeight: FontWeight.bold,
-            ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomAppBar(),
+              const FeaturedBooksListView(),
+              const Gap(50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text(
+                  'Best Seller',
+                  style: Styles.textStyle18.copyWith(
+                    fontFamily: kMontserrat,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Gap(20),
+            ],
           ),
-          const Gap(20),
-          const BestSellerListViewItems(),
-        ],
-      ),
+        ),
+        const SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: BestSellerListView(),
+          ),
+        ),
+      ],
     );
   }
 }
