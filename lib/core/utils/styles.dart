ec693cf805baa580809ad2e9e38eee1e2545e78b
m9stafa05 <m9stafa05@gmail.com>
2025-07-08 21:51:41 +0300

    Update the styling

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,9 @@
 import 'package:bookly_app/constants.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 abstract class Styles {
-    static const textStyle14 = TextStyle(
+  static const textStyle14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
     fontFamily: kGTSectraFineBlack,
@@ -10,22 +11,20 @@ abstract class Styles {
   static const textStyle16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
-    fontFamily: kGTSectraFineBlack,
+    color: Colors.white,
   );
   static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
     fontFamily: kGTSectraFineBlack,
   );
-    static const textStyle20 = TextStyle(
+  static const textStyle20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
-    fontFamily: kGTSectraFineBlack,
   );
   static const textStyle30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal,
     fontFamily: kGTSectraFineBlack,
   );
-
 }
