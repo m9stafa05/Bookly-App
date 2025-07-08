85bd953d2c0867113ad2e889cab9dfbd7473d7b3
m9stafa05 <m9stafa05@gmail.com>
2025-07-08 19:16:10 +0300

    Correct the import files

--- lib/features/home/presentation/views/widgets/howe_view_body.dart
+++ lib/features/home/presentation/views/widgets/howe_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/featured_bokks_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -18,8 +18,8 @@ class HoweViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           Gap(50),
-          Text('Best Seller', style: Styles.titleMedium),
-          Gap(20),
+          Text('Best Seller', style: Styles.textStyle18),
+          Gap(10),
           BestSellerListViewItems(),
         ],
       ),
