96522945ada1bf8879d5efccecc5d37df881a4ff
m9stafa05 <m9stafa05@gmail.com>
2025-07-08 21:51:24 +0300

    Add const for Montserrat fonts

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100B20);
 const kTransition = Duration(milliseconds: 250);
 const kGTSectraFineBlack = 'GT-Sectra-Fine-Black';
+const kMontserrat = 'Montserrat-VariableFont_wght';
