e685936ddfd93884618fb0a00d078c86889145e6
m9stafa05 <m9stafa05@gmail.com>
2025-07-11 20:54:30 +0300

    Fix Size

--- lib/features/home/presentation/views/widgets/custom_books_details_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_books_details_app_bar.dart
@@ -8,10 +8,7 @@ class CustomBookDetailsAppBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.close, size: 35),
-        ),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.close)),
         IconButton(
           onPressed: () {},
           icon: const Icon(Icons.shopping_cart_checkout_outlined),
