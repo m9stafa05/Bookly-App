d04b0d138d404f2457e5d872639a1b5e56b6855d
m9stafa05 <m9stafa05@gmail.com>
2025-07-11 19:47:06 +0300

    Create a route for the screens routes

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  // GoRouter configuration
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => const HomeView(),
+      ),
+    ],
+  );
+}
