52a542ab1ea868fe273510b9239624a1acc0201e
m9stafa05 <m9stafa05@gmail.com>
2025-07-11 20:33:18 +0300

    display the app bar in the view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_books_details_app_bar.dart';
+import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(children: [CustomBookDetailsAppBar()]),
+    );
   }
 }
