75482092891c0891d25310fbea38401bda835318
m9stafa05 <m9stafa05@gmail.com>
2025-07-11 22:06:55 +0300

    Update font family in style

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -15,7 +15,6 @@ abstract class Styles {
   static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
-    fontFamily: kGTSectraFineBlack,
   );
   static const textStyle20 = TextStyle(
     fontSize: 20,
