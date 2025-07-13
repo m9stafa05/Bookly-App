c84f382f668b00d94b11c432f8ae15c616ca1519
m9stafa05 <m9stafa05@gmail.com>
2025-07-14 02:13:33 +0300

    Update the models to fix the expiations

--- lib/features/home/data/models/book_model/image_links.dart
+++ lib/features/home/data/models/book_model/image_links.dart
@@ -1,15 +1,19 @@
 import 'package:equatable/equatable.dart';
 
 class ImageLinks extends Equatable {
-  final String? smallThumbnail;
-  final String? thumbnail;
+  final String smallThumbnail;
+  final String thumbnail;
 
-  const ImageLinks({this.smallThumbnail, this.thumbnail});
+  const ImageLinks({
+    required this.smallThumbnail,
+    required this.thumbnail,
+  });
 
-  factory ImageLinks.fromJson(Map<String, dynamic> json) => ImageLinks(
-    smallThumbnail: json['smallThumbnail'] as String?,
-    thumbnail: json['thumbnail'] as String?,
-  );
+  factory ImageLinks.fromJson(Map<String, dynamic> json) =>
+      ImageLinks(
+        smallThumbnail: json['smallThumbnail'] as String,
+        thumbnail: json['thumbnail'] as String,
+      );
 
   Map<String, dynamic> toJson() => {
     'smallThumbnail': smallThumbnail,
