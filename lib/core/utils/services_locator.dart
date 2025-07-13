66419f823ae70e5c988d0011c31f128e6754421e
m9stafa05 <m9stafa05@gmail.com>
2025-07-14 00:57:05 +0300

    Create service locator

---
+++ lib/core/utils/services_locator.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_app/core/utils/api_services.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setup() {
+  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(apiServices: getIt.get<ApiServices>()),
+  );
+}
