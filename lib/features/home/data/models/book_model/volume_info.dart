778453ebd10ea76ddaa53ddb8a7526a6bcb24d00
m9stafa05 <m9stafa05@gmail.com>
2025-07-14 01:47:52 +0300

    Fix config

--- lib/features/home/data/models/book_model/volume_info.dart
+++ lib/features/home/data/models/book_model/volume_info.dart
@@ -48,37 +48,48 @@ class VolumeInfo extends Equatable {
     this.canonicalVolumeLink,
   });
 
-  factory VolumeInfo.fromJson(Map<String, dynamic> json) => VolumeInfo(
-    title: json['title'] as String?,
-    authors: json['authors'] as List<String>?,
-    publisher: json['publisher'] as String?,
-    publishedDate: json['publishedDate'] as String?,
-    description: json['description'] as String?,
-    industryIdentifiers: (json['industryIdentifiers'] as List<dynamic>?)
-        ?.map((e) => IndustryIdentifier.fromJson(e as Map<String, dynamic>))
-        .toList(),
-    readingModes: json['readingModes'] == null
-        ? null
-        : ReadingModes.fromJson(json['readingModes'] as Map<String, dynamic>),
-    pageCount: json['pageCount'] as int?,
-    printType: json['printType'] as String?,
-    categories: json['categories'] as List<String>?,
-    maturityRating: json['maturityRating'] as String?,
-    allowAnonLogging: json['allowAnonLogging'] as bool?,
-    contentVersion: json['contentVersion'] as String?,
-    panelizationSummary: json['panelizationSummary'] == null
-        ? null
-        : PanelizationSummary.fromJson(
-            json['panelizationSummary'] as Map<String, dynamic>,
-          ),
-    imageLinks: json['imageLinks'] == null
-        ? null
-        : ImageLinks.fromJson(json['imageLinks'] as Map<String, dynamic>),
-    language: json['language'] as String?,
-    previewLink: json['previewLink'] as String?,
-    infoLink: json['infoLink'] as String?,
-    canonicalVolumeLink: json['canonicalVolumeLink'] as String?,
-  );
+  factory VolumeInfo.fromJson(Map<String, dynamic> json) =>
+      VolumeInfo(
+        title: json['title'] as String?,
+        authors: (json['authors'] as List<dynamic>?)?.cast<String>(),
+        publisher: json['publisher'] as String?,
+        publishedDate: json['publishedDate'] as String?,
+        description: json['description'] as String?,
+        industryIdentifiers:
+            (json['industryIdentifiers'] as List<dynamic>?)
+                ?.map(
+                  (e) => IndustryIdentifier.fromJson(
+                    e as Map<String, dynamic>,
+                  ),
+                )
+                .toList(),
+        readingModes: json['readingModes'] == null
+            ? null
+            : ReadingModes.fromJson(
+                json['readingModes'] as Map<String, dynamic>,
+              ),
+        pageCount: json['pageCount'] as int?,
+        printType: json['printType'] as String?,
+        categories: (json['categories'] as List<dynamic>?)
+            ?.cast<String>(),
+        maturityRating: json['maturityRating'] as String?,
+        allowAnonLogging: json['allowAnonLogging'] as bool?,
+        contentVersion: json['contentVersion'] as String?,
+        panelizationSummary: json['panelizationSummary'] == null
+            ? null
+            : PanelizationSummary.fromJson(
+                json['panelizationSummary'] as Map<String, dynamic>,
+              ),
+        imageLinks: json['imageLinks'] == null
+            ? null
+            : ImageLinks.fromJson(
+                json['imageLinks'] as Map<String, dynamic>,
+              ),
+        language: json['language'] as String?,
+        previewLink: json['previewLink'] as String?,
+        infoLink: json['infoLink'] as String?,
+        canonicalVolumeLink: json['canonicalVolumeLink'] as String?,
+      );
 
   Map<String, dynamic> toJson() => {
     'title': title,
@@ -86,7 +97,9 @@ class VolumeInfo extends Equatable {
     'publisher': publisher,
     'publishedDate': publishedDate,
     'description': description,
-    'industryIdentifiers': industryIdentifiers?.map((e) => e.toJson()).toList(),
+    'industryIdentifiers': industryIdentifiers
+        ?.map((e) => e.toJson())
+        .toList(),
     'readingModes': readingModes?.toJson(),
     'pageCount': pageCount,
     'printType': printType,
@@ -129,7 +142,8 @@ class VolumeInfo extends Equatable {
       publisher: publisher ?? this.publisher,
       publishedDate: publishedDate ?? this.publishedDate,
       description: description ?? this.description,
-      industryIdentifiers: industryIdentifiers ?? this.industryIdentifiers,
+      industryIdentifiers:
+          industryIdentifiers ?? this.industryIdentifiers,
       readingModes: readingModes ?? this.readingModes,
       pageCount: pageCount ?? this.pageCount,
       printType: printType ?? this.printType,
@@ -137,12 +151,14 @@ class VolumeInfo extends Equatable {
       maturityRating: maturityRating ?? this.maturityRating,
       allowAnonLogging: allowAnonLogging ?? this.allowAnonLogging,
       contentVersion: contentVersion ?? this.contentVersion,
-      panelizationSummary: panelizationSummary ?? this.panelizationSummary,
+      panelizationSummary:
+          panelizationSummary ?? this.panelizationSummary,
       imageLinks: imageLinks ?? this.imageLinks,
       language: language ?? this.language,
       previewLink: previewLink ?? this.previewLink,
       infoLink: infoLink ?? this.infoLink,
-      canonicalVolumeLink: canonicalVolumeLink ?? this.canonicalVolumeLink,
+      canonicalVolumeLink:
+          canonicalVolumeLink ?? this.canonicalVolumeLink,
     );
   }
 
