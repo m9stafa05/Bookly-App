78f483ca3d380dc8775ba018876b97d700338952
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 22:29:48 +0300

    Implement book search

---
+++ lib/features/search/data/repo/search_repo_impl.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
+import 'package:bookly_app/features/search/data/repo/search_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  final ApiServices apiServices;
+
+  SearchRepoImpl({required this.apiServices});
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSearchBooks({
+    required String subject,
+  }) async {
+    try {
+      var data = await apiServices.get(
+        endPinot:
+            'volumes?q=$subject&filter=free-ebooks&orderBy=newest',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
