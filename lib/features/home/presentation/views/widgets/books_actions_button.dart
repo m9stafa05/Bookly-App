3ae718d89393edb6cea78e53723123b31599d428
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 19:59:47 +0300

    Launch book in browser

--- lib/features/home/presentation/views/widgets/books_actions_button.dart
+++ lib/features/home/presentation/views/widgets/books_actions_button.dart
@@ -1,16 +1,20 @@
 import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:bookly_app/core/widgets/show_snack_bar.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BooksActionButton extends StatelessWidget {
-  const BooksActionButton({super.key});
+  const BooksActionButton({super.key, required this.bookModel});
+  final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
-          Expanded(
+          const Expanded(
             child: CustomButton(
               backgroundColor: Colors.white,
               textColor: Colors.black,
@@ -24,9 +28,33 @@ class BooksActionButton extends StatelessWidget {
           ),
           Expanded(
             child: CustomButton(
-              backgroundColor: Color(0xffEF8262),
+              onPressed: () async {
+                final Uri url = Uri.parse(
+                  bookModel.volumeInfo.previewLink ?? '',
+                );
+                try {
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    ShowSnackBar(
+                      message:
+                          'Sorry, could not open the preview link.',
+                      // ignore: use_build_context_synchronously
+                      context: context,
+                    );
+                  }
+                } catch (e) {
+                  ShowSnackBar(
+                    message:
+                        'An error occurred while trying to open the link.',
+                    // ignore: use_build_context_synchronously
+                    context: context,
+                  );
+                }
+              },
+              backgroundColor: const Color(0xffEF8262),
               textColor: Colors.white,
-              borderRadius: BorderRadius.only(
+              borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(10),
                 bottomRight: Radius.circular(10),
               ),
