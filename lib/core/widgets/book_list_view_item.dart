e1745f242dca90deb0944212dd4dd4ed64534601
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 21:41:56 +0300

    Move files to core

--- lib/core/widgets/book_list_view_item.dart
+++ lib/core/widgets/book_list_view_item.dart
@@ -3,7 +3,7 @@ import 'package:bookly_app/core/models/book_model/book_model.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/core/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
