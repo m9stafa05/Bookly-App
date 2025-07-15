fcae95173ad2867a106db276213edda246f700a2
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 19:07:00 +0300

    Send the data Model With widget

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_books_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
@@ -5,22 +6,23 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
-  const BookDetailsViewBody({super.key});
+  const BookDetailsViewBody({super.key, required this.bookModel});
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
             child: Column(
               children: [
-                CustomBookDetailsAppBar(),
-                Gap(10),
-                BookDetailsSection(),
-                Expanded(child: Gap(40)),
-                SimilarBooksSection(),
+                const CustomBookDetailsAppBar(),
+                const Gap(10),
+                BookDetailsSection(bookModel: bookModel),
+                const Expanded(child: Gap(40)),
+                const SimilarBooksSection(),
               ],
             ),
           ),
