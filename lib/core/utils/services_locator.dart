872bb9538e077f2b8b679001578b9be9b9607ca0
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 22:58:07 +0300

    Add SearchRepoImpl

--- lib/core/utils/services_locator.dart
+++ lib/core/utils/services_locator.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:bookly_app/features/search/data/repo/search_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -8,5 +9,8 @@ void setupServiceLocator() {
   getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
   getIt.registerSingleton<HomeRepoImpl>(
     HomeRepoImpl(apiServices: getIt.get<ApiServices>()),
+  );
+  getIt.registerSingleton<SearchRepoImpl>(
+    SearchRepoImpl(apiServices: getIt.get<ApiServices>()),
   );
 }
