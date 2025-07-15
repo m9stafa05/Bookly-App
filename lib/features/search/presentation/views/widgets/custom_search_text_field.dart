b91b6359fc6a7f230081d2a5dd68831e34d00ea3
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 23:00:44 +0300

    refactor to apply the search

--- lib/features/search/presentation/views/widgets/custom_search_text_field.dart
+++ lib/features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -2,21 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomSearchTextField extends StatelessWidget {
-  const CustomSearchTextField({super.key});
-
+  const CustomSearchTextField({
+    super.key,
+    this.onPressed,
+    required this.searchController,
+  });
+  final void Function()? onPressed;
+  final TextEditingController searchController;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: searchController,
       decoration: InputDecoration(
         hintText: 'Search',
         enabledBorder: outLineInputBorder(),
         focusedBorder: outLineInputBorder(),
         suffixIcon: IconButton(
-          onPressed: () {},
+          onPressed: onPressed,
           icon: const Icon(
             FontAwesomeIcons.magnifyingGlass,
             size: 24,
-            color: Color.fromARGB(44, 255, 255, 255),
+            color: Color(0xF2CECECE),
           ),
         ),
       ),
