32162c914130405d10894ae7b9beb20d6709ab26
m9stafa05 <m9stafa05@gmail.com>
2025-07-15 18:08:33 +0300

    Implement fetch smiler books

--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -30,13 +30,35 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+  Future<Either<Failure, List<BookModel>>>
+  fetchFeaturedBooks() async {
+    try {
+      var data = await apiServices.get(
+        endPinot: 'volumes?q=programming&filter=free-ebooks',
+      );
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSmilerBooks({
+    required String category,
+  }) async {
     try {
       var data = await apiServices.get(
         endPinot:
-            'volumes?q=programming&filter=free-ebooks',
+            'volumes?q=programming&filter=free-ebooks&Sorting=relevance',
       );
-
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
